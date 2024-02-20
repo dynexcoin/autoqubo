65d944735e5197c01ddc89215e7553470ff213f2
Dynex [DNX] <113523376+dynexcoin@users.noreply.github.com>
2024-02-20 15:53:45 +0500

    Update e1.py

--- examples/e1.py
+++ examples/e1.py
@@ -12,7 +12,7 @@ def ff(a, b):
     return 2 * a + 3 * b
 
 
-q, offset = SamplingCompiler.generate_qubo_matrix(ff, s.size, s)
+q, offset = SamplingCompiler.generate_qubo_matrix(fitness_function=ff, input_size=s.size, searchspace=s)
 
 print(q)
 
@@ -21,5 +21,5 @@ x = s.encode({'a': 3, 'b': 6})
 print(s.decode(x))
 print(s.decode_dict(x))
 
-sampleset = dynex.sample_qubo(q, offset, num_reads=1024, annealing_time=200)
-print(sampleset)
+print(Utils.energy(q, x, offset=offset))
+print(ff(3, 6))
